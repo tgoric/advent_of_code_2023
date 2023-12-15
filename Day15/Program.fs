43999bdb8c6ff25d4f5c6836f00590641c399008
Tim Goric <timothy.goric@himco.com>
2023-12-15 16:28:54 -0500

    Add Day 15 part 1 solution

---
+++ Day15/Program.fs
@@ -0,0 +1,11 @@
+﻿open System.IO
+
+File.ReadAllText("input.txt").Split(",") 
+    |> Seq.map (fun x -> 
+            let mutable curr = 0
+            for i in x do
+                curr <- ((curr + (i |> int)) * 17) % 256
+            curr
+        )
+    |> Seq.sum
+    |> printfn "%i"
